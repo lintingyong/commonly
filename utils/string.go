3064a1172fc364e460d50338e395a8cc09185aa8
oneminuter <1050086935@qq.com>
2019-06-28 17:54:48 +0800

    新增util 获取随机字符串

---
+++ utils/string.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+/*
+	随机获取字符串
+	l: 要获取字符串的长度
+	返回：字符串
+ */
+func GetRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	var result []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
