93ff5adf59603ad275ca77469dcd7a7efa4e28b3
oneminuter <1050086935@qq.com>
2019-07-10 23:49:36 +0800

    增加字符串转字节数组(避免字符串拷贝)方法封装

--- utils/string.go
+++ utils/string.go
@@ -3,13 +3,14 @@ package utils
 import (
 	"math/rand"
 	"time"
+	"unsafe"
 )
 
 /*
 	随机获取字符串
 	l: 要获取字符串的长度
 	返回：字符串
- */
+*/
 func GetRandomString(l int) string {
 	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	bytes := []byte(str)
@@ -19,4 +20,12 @@ func GetRandomString(l int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+// Str2bytes 字符串转字节数组(避免字符串拷贝)
+func Str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+
+	return *(*[]byte)(unsafe.Pointer(&h))
 }
